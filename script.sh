f89591e4a785644d4a7c6591731804aa7fc7fdd0
ai-tonchev <147974544+ai-tonchev@users.noreply.github.com>
2025-11-05 18:55:05 +0200

    Delete script.sh

--- script.sh
+++
@@ -1,6 +0,0 @@
-scp -i aktoviz-keys.pem -r  app ubuntu@51.21.32.24:~/
-scp -i aktoviz-keys.pem -r  app/Dockerfile ubuntu@51.21.32.24:~/app
-scp -i aktoviz-keys.pem -r  app/requirements.txt ubuntu@51.21.32.24:~/app
-scp -i aktoviz-keys.pem -r  app/docker-compose.yml ubuntu@51.21.32.24:~/
-scp -i aktoviz-keys.pem -r  app/app.py ubuntu@51.21.32.24:~/app
-scp -i aktoviz-keys.pem -r  app/docker-compose.yml ubuntu@51.21.32.24:~/app
